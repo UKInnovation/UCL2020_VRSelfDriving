8948a367d3d24daefc1eb49c3b07e4a4d7900ca5
Xuyou Cheng <yoyoucheng@outlook.com>
2020-03-09 19:22:35 +0000

    Implemented simple Navigation Map Choose Point Algorithm

--- Assets/NavigationCanvas.cs
+++ Assets/NavigationCanvas.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using YoyouOculusFramework;
 
-namespace YoyouOculusFramework
+namespace VehicleNavigation
 {
     public class NavigationCanvas : MonoBehaviour
     {
@@ -12,12 +13,18 @@ namespace YoyouOculusFramework
         private Camera MapCamera;
 
         public GameObject prefeb;
+        [SerializeField]
+        private List<Transform> vertices;
         /// <summary>
         /// Awake is called when the script instance is being loaded.
         /// </summary>
         void Awake()
         {
             initializeGrid();
+        }
+
+        void Start() {
+            NavigateTo(new Vector2(300,300));
         }
 
         private void initializeGrid()
@@ -45,7 +52,6 @@ namespace YoyouOculusFramework
                     grid.transform.parent = this.transform;
                     grid.GetComponent<RectTransform>().localPosition = currentGridPos;
                     grid.OnEnterActionZone.AddListener(delegate{NavigateTo(currentGridPos);});
-                    NavigateTo(currentGridPos);
                     currentGridPos += GoRight;
                 }
                 currentGridPos.x = startPosition.x;
@@ -65,15 +71,33 @@ namespace YoyouOculusFramework
             Vector3 CameraHeadingDirection3d = MapCamera.transform.rotation.eulerAngles;
             // Vector2 CameraHeadingDirection2d = new Vector2(CameraHeadingDirection3d.x, CameraHeadingDirection3d.z);
 
-            Debug.Log(relativePos);
+            // Debug.Log(relativePos);
             float angle = Mathf.Atan(relativePos.x/relativePos.y);
             if(relativePos.y < 0)
             {
                 angle = Mathf.PI + angle;
             }
+
             angle = angle + (CameraHeadingDirection3d.y * Mathf.PI)/180;
             Vector3 realWorldPos = transform.position + (new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * realWorldrelativePos.magnitude);
-            Instantiate(prefeb, realWorldPos, new Quaternion());
+            
+
+            float shortestDistance = float.MaxValue;
+            Transform closestVertex = null;
+            foreach(Transform vertex in vertices)
+            {
+                float Distance = Vector3.Distance(vertex.position, realWorldPos);
+                if(Distance < shortestDistance)
+                {
+                    Debug.Log(vertex.gameObject.name);
+                    shortestDistance = Distance;
+                    closestVertex = vertex;
+                }
+            }
+
+            if(closestVertex != null){
+                Destroy(closestVertex.gameObject);
+            }
         }
     }
 }
