b1624c0391a3b65e1669866a8b7257a08cd52465
Xuyou <yoyoucheng@outlook.com>
2020-03-23 21:33:26 +0000

    Fixed tire car not changing bar, adjust position of game element in scene

---
+++ Assets/MusicButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicButton : MonoBehaviour
+{
+    private Text text;
+    private int index;
+    private void Awake()
+    {
+        text = this.GetComponentInChildren<Text>();
+    }
+
+    public void OnClick()
+    {
+        if (index == 0)
+        {
+            text.text = "Play Music";
+            index = 1;
+        }
+        else
+        {
+            text.text = "Stop Music";
+            index = 0;
+        }
+    }
+}
--- Assets/Scripts/Car Selection Part/BGM_demo.cs
+++ Assets/Scripts/Car Selection Part/BGM_demo.cs
@@ -7,42 +7,24 @@ using UnityEngine.UI;
 public class BGM_demo : MonoBehaviour
 {
     public AudioSource audioSource;
-    public Slider slider;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource.Play();
-        slider.value = 1;
         audioSource.loop = true;
-        audioSource.volume = 1f;
+        audioSource.volume = 0.5f;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //change volume of background music
-        audioSource.volume = slider.value;
-    }
 
     //start or stop the music
     public void play_stop_music()
     {
-        Debug.Log("s");
         if (!audioSource.isPlaying)
         {
             audioSource.Play();
         }
         else
-        {
-            audioSource.Stop();
-        }
-    }
-
-    //pause
-    public void pause_music()
-    {
-        if (audioSource.isPlaying)
         {
             audioSource.Pause();
         }
